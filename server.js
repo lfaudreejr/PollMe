d3de80ddcda93570aaea0394624a2709bd659b93
Larry Faudree <lfaudrejr@hotmail.com>
2017-07-16 10:46:55 -0400

    initial server structure established. connected to mongolab

---
+++ server.js
@@ -0,0 +1,64 @@
+/**
+ * Dependencies
+ */
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const cors = require("cors");
+
+const config = require("./server/config");
+
+/**
+ * MongoDB
+ */
+mongoose.connect(config.MONGO_URI, { useMongoClient: true });
+const mongoDB = mongoose.connection;
+
+mongoDB.on("error", () => {
+  console.error(
+    "MongoDB Connectiong Error. Please make sure that",
+    config.MONGO_URI,
+    "is running."
+  );
+});
+mongoDB.on("open", function callback() {
+  console.info("Connected to MongoDO:", config.MONGO_URI);
+});
+
+/**
+ * App
+ */
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(cors());
+
+// Set PORT
+const port = process.env.PORT || "8080";
+app.set("port", port);
+
+// Set static path to Angular app in dist
+// Dont run in dev
+if (process.env.NODE_ENV !== "dev") {
+  app.use("/", express.static(path.join(__dirname, "/dist")));
+}
+
+/**
+ * Routes
+ */
+require("./server/api")(app, config);
+
+// Pass routing to Angular
+// Dont run in dev
+if (process.env.NODE_ENV !== "dev") {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+}
+
+/**
+ * Server
+ */
+app.listen(port, () => console.log(`Server listening on port ${port}`));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
