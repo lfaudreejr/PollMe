65c5d16ecdd1163593fed341b991a9aab175e5e2
Larry Faudree <lfaudrejr@hotmail.com>
2017-07-22 14:39:04 -0400

    deploy

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: "lfaudreejr.auth0.com",
+  AUTH0_API_AUDIENCE: "http://localhost:8083/api/",
+  MONGO_URI: "mongodb://larry:work4you@ds161162.mlab.com:61162/pollme"
+};
