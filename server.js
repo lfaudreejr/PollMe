94fff3cf4a7fde89a4b777ea70a163f9b575066e
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-13 02:41:27 -0500

    updated styles

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const cors = require("cors");
+const lusca = require('lusca');
+const logger = require('morgan');
+const mongo = require('connect-mongo');
+const session = require('express-session');
+const compression = require('compression');
+const favicon = require('serve-favicon');
 require("dotenv").config();
 
 const config = require("./server/config");
+
+const MongoStore = mongo(session);
 
 /**
  * MongoDB
@@ -23,6 +30,7 @@ mongoDB.on("error", () => {
     config.MONGO_URI,
     "is running."
   );
+  process.exit();
 });
 mongoDB.on("open", function callback() {
   console.info("Connected to MongoDO:", config.MONGO_URI);
@@ -33,18 +41,41 @@ mongoDB.on("open", function callback() {
  */
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("X-HTTP-Method-Override"));
-app.use(cors());
-
 // Set PORT
 const port = process.env.PORT || 8080;
 app.set("port", port);
 
+app.use(compression());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("X-HTTP-Method-Override"));
+
+if(process.env.NODE_ENV !== 'production') {
+  const cors = require('cors')
+  app.use(cors());
+}
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESH_SECRET,
+  store: new MongoStore({
+    url: process.env.MONGO_URI,
+    autoReconnect: true
+  })
+}))
+
 // Set static path to Angular app in dist
 // Dont run in dev
 if (process.env.NODE_ENV !== "dev") {
+  app.use(lusca.csrf());
+  app.use(lusca.xframe("SAMEORIGIN"));
+  app.use(lusca.xssProtection(true));
+  app.use(lusca.nosniff());
+  app.use(lusca.referrerPolicy('same-origin'));
+
+  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use("/", express.static(path.join(__dirname, "./dist")));
 }
 
--- server/api.js
+++ server/api.js
@@ -1,12 +1,12 @@
-const jwt = require("express-jwt");
-const jwks = require("jwks-rsa");
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
 
-const Poll = require("./models/Poll");
+const Poll = require('./models/Poll')
 
 /**
  * Auth middleware
  */
-module.exports = function(app, config) {
+module.exports = function (app, config) {
   // Middleware for auth
   const jwtCheck = jwt({
     secret: jwks.expressJwtSecret({
@@ -17,47 +17,49 @@ module.exports = function(app, config) {
     }),
     aud: config.AUTH0_API_AUDIENCE,
     issuer: `https://${config.AUTH0_DOMAIN}/`,
-    algorithm: "RS256"
-  });
+    algorithm: 'RS256'
+  })
 
   /**
    * API Routes
    */
 
   // GET API Root
-  app.get("/api/", (req, res) => {
-    res.send("API works");
-  });
+  app.get('/api/', (req, res) => {
+    res.send('API works')
+  })
   // GET all polls listed, limit 20
-  app.get("/api/polls", (req, res) => {
+  app.get('/api/polls', (req, res) => {
     Poll.find({}, (err, polls) => {
-      let pollArr = [];
-      if (err) {
-        return res.status(500).send({ message: err.message });
+      let pollArr = []
+      {
+        if (err) {
+          return res.status(500).send({ message: err.message })
+        }
+        if (polls) {
+          polls.forEach((poll) => {
+            pollArr.push(poll)
+          })
+        }
       }
-      if (polls) {
-        polls.forEach(poll => {
-          pollArr.push(poll);
-        });
-      }
-      res.send(pollArr);
-    });
-  });
+      res.send(pollArr)
+    })
+  })
   // GET poll details
-  app.get("/api/poll/:id", (req, res) => {
+  app.get('/api/poll/:id', (req, res) => {
     Poll.findById(req.params.id, (err, poll) => {
       if (err) {
-        return res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message })
       }
       if (!poll) {
-        return res.status(400).send({ message: "Poll not found" });
+        return res.status(400).send({ message: 'Poll not found' })
       }
-      res.send(poll);
-    });
-  });
+      res.send(poll)
+    })
+  })
 
   // POST a new poll
-  app.post("/api/poll/new", (req, res) => {
+  app.post('/api/poll/new', (req, res) => {
     Poll.findOne(
       {
         title: req.body.title,
@@ -65,12 +67,10 @@ module.exports = function(app, config) {
       },
       (err, existingPoll) => {
         if (err) {
-          return res.status(500).send({ message: err.message });
+          return res.status(500).send({ message: err.message })
         }
         if (existingPoll) {
-          return res
-            .status(409)
-            .send({ message: "You already created a poll with this title" });
+          return res.status(409).send({ message: 'You already created a poll with this title' })
         }
 
         const poll = new Poll({
@@ -78,81 +78,77 @@ module.exports = function(app, config) {
           options: req.body.options,
           owner: req.body.owner,
           voters: []
-        });
-        poll.save(err => {
+        })
+        poll.save((err) => {
           if (err) {
-            return res.status(500).send({ message: err.message });
+            return res.status(500).send({ message: err.message })
           }
-          res.send(poll);
-        });
+          res.send(poll)
+        })
       }
-    );
-  });
+    )
+  })
 
   // POST a vote to existing Poll
-  app.post("/api/poll/:id", (req, res) => {
+  app.post('/api/poll/:id', (req, res) => {
     Poll.findById(req.params.id, (err, foundPoll) => {
       if (err) {
-        return res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message })
       }
       if (!foundPoll) {
-        return res.status(400).message({ message: "Poll not found." });
+        return res.status(400).message({ message: 'Poll not found.' })
       }
 
       for (let i = 0; i < foundPoll.options.length; i++) {
         if (foundPoll.options[i].title === req.body.option) {
-          foundPoll.options[i].count = foundPoll.options[i].count + 1;
+          foundPoll.options[i].count = foundPoll.options[i].count + 1
         }
       }
-      if (process.env.NODE_ENV !== "dev") {
+      if (process.env.NODE_ENV !== 'dev') {
         for (let i = 0; i < foundPoll.voters.length; i++) {
           if (foundPoll.voters[i] === req.body.voter) {
-            return res
-              .status(403)
-              .send({ message: "You have already voted on this poll." });
+            return res.status(403).send({ message: 'You have already voted on this poll.' })
           }
         }
       }
-      foundPoll.voters.push(req.body.voter);
-      foundPoll.save(err => {
+      foundPoll.voters.push(req.body.voter)
+      foundPoll.save((err) => {
         if (err) {
-          return res.status(500).send({ message: err.message });
+          return res.status(500).send({ message: err.message })
         }
-        res.send(foundPoll);
-      });
-    });
-  });
+        res.send(foundPoll)
+      })
+    })
+  })
 
   // DELETE a Poll option
-  app.delete("/api/poll/:id", (req, res) => {
+  app.delete('/api/poll/:id', (req, res) => {
     Poll.findById(req.params.id, (err, foundPoll) => {
       if (err) {
-        return res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message })
       }
       if (!foundPoll) {
-        return res.status(400).send({ message: "Poll not found." });
+        return res.status(400).send({ message: 'Poll not found.' })
       }
-      foundPoll.remove(err => {
+      foundPoll.remove((err) => {
         if (err) {
-          return res.status(500).send({ message: err.message });
+          return res.status(500).send({ message: err.message })
         }
-        res.status(200).send({ message: "Poll successfully deleted." });
-      });
-    });
-  });
+        res.status(200).send({ message: 'Poll successfully deleted.' })
+      })
+    })
+  })
 
   // GET Polls for a user
-  app.get("/api/:user/polls", (req, res) => {
+  app.get('/api/:user/polls', (req, res) => {
     Poll.find({ owner: req.params.user }, (err, foundPolls) => {
       if (err) {
-        return res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message })
       }
       if (!foundPolls) {
-        return res
-          .status(400)
-          .send({ message: "No Polls found for this user." });
+        return res.status(400).send({ message: 'No Polls found for this user.' })
       }
-      res.send(foundPolls);
-    });
-  });
-};
+      res.send(foundPolls)
+    })
+  })
+}
